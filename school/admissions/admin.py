a2cfeb104bba7a07863ef6bf72ba7169b437503d
Sandeep <sandeepl79@gmail.com>
2014-03-26 00:09:17 +0530

    adding model definitions

--- school/admissions/admin.py
+++ school/admissions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from admissions.models import ApplicationFormTemplate
+
+
+class ApplicationFormTemplateAdmin(admin.ModelAdmin):
+    model = ApplicationFormTemplate
+
+
+admin.site.register(ApplicationFormTemplate, ApplicationFormTemplateAdmin)
