c3cbe425d1e7815b10115f52a7120b6fca31debb
Sandeep <sandeepl79@gmail.com>
2014-03-25 23:22:57 +0530

    added django-registration app added django-registration-defaults  template app

--- school/school/settings.py
+++ school/school/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from registration_defaults.settings import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
     'admissions',
 )
 
@@ -81,3 +84,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (REGISTRATION_TEMPLATE_DIR,)
--- school/school/urls.py
+++ school/school/urls.py
@@ -1,12 +1,35 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import *
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'school.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'school.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
+                       #override the default urls
+                       url(r'^accounts/password/change/$',
+                           auth_views.password_change,
+                           name='password_change'),
+                       url(r'^accounts/password/change/done/$',
+                           auth_views.password_change_done,
+                           name='password_change_done'),
+                       url(r'^accounts/password/reset/$',
+                           auth_views.password_reset,
+                           name='password_reset'),
+                       url(r'^accounts/password/reset/done/$',
+                           auth_views.password_reset_done,
+                           name='password_reset_done'),
+                       url(r'^accounts/password/reset/complete/$',
+                           auth_views.password_reset_complete,
+                           name='password_reset_complete'),
+                       url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+                           auth_views.password_reset_confirm,
+                           name='password_reset_confirm'),
+
+                       #and now add the registration urls
+                       url(r'^accounts/', include('registration.backends.default.urls')),
 )
